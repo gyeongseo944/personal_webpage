5e1907e286b3de81f057fe30f0ca2b59f2b896a2
DESKTOP-8H3R7HO\gyeon <gyeongseo944@gmail.com>
2022-06-23 01:53:40 +0900

    gsap install

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
-import gsap from "gsap-trial";
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
+import gsap from "gsap";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
 import "./Logo.scss";
 import logoG from "../../../assets/images/logo_G2.png";
 
